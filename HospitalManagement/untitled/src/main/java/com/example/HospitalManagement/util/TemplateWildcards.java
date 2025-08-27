b222c0a5ebf7d853c18f41c74db66f8a9b92853a
florentmuqolli <muqolliflorentt@gmail.com>
2025-08-27 17:57:10 +0200

    template configuring

---
+++ HospitalManagement/untitled/src/main/java/com/example/HospitalManagement/util/TemplateWildcards.java
@@ -0,0 +1,34 @@
+package com.example.HospitalManagement.util;
+
+public abstract class TemplateWildcards {
+
+    public final static String SYSTEM_LOGO = "systemLogo";
+
+    public final static String PATIENT_NAME = "patientName";
+    public final static String PATIENT_PHONE_NUMBER = "phoneNumber";
+    public final static String PATIENT_EMAIL = "patientEmail";
+    public final static String PATIENT_ADDRESS = "patientAddress";
+    public final static String PATIENT_BIRTH = "dateOfBirth";
+    public final static String PATIENT_AGE = "age";
+    public final static String PATIENT_ID = "patientId";
+    public final static String PATIENT_ROOM_NAME = "patientRoomName";
+    public final static String PATIENT_ROOM_ID = "patientRoomId";
+
+//for nurse
+    public final static String NURSE_NAME = "nurseName";
+    public final static String NURSE_DEPARTAMENT = "department";
+    public final static String NURSE_SHIFT_TIMING = "shiftTiming";
+    public final static String NURSE_ID = "nurseId";
+    public final static String NURSE_CONTACT = "contactNumber";
+    public final static String NURSE_ADDRESS = "address";
+
+    //for doctor
+    public final static String DOCTOR_NAME = "doctorName";
+    public final static String DOCTOR_DEPARTAMENT = "department";
+    public final static String DOCTOR_SPECIALIZATION = "specialization";
+    public final static String DOCTOR_ID = "doctorId";
+    public final static String DOCTOR_CONTACT = "officePhone";
+    public final static String DOCTOR_ADDRESS = "address";
+
+
+}
